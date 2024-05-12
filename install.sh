0c046d1f5381ae539f125e56a9870be457565e9d
pranava <98644231+pranava-mk@users.noreply.github.com>
2024-05-12 10:07:42 +0530

    base

---
+++ install.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+
+# Install Terminus Fonts
+sudo apt install fonts-terminus
+
+# Set the font to Terminus Fonts
+setfont /usr/share/consolefonts/Uni3-TerminusBold28x14.psf.gz
+
+# Clear the screen
+clear
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Install nala
+apt install nala -y
+
+
+# Making .config and Moving config files and wallpapers to Pictures
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/.icons
+mkdir -p /home/$username/.themes
+mkdir -p /home/$username/Pictures
+mkdir -p /home/$username/Pictures/wallpapers
+cp -R dotconfig/* /home/$username/.config/
+cp -R doticons/* /home/$username/.icons/
+cp -R dotthemes/* /home/$username/.themes/
+cp bg.jpg /home/$username/Pictures/wallpapers/
+mv user-dirs.dirs /home/$username/.config
+chown -R $username:$username /home/$username
+
+# Installing Some Programs
+nala install wget unzip zoxide neofetch -y 
+
+
+#Installing fonts (also moving fonts from dotfonts to .fonts)
+cd $builddir 
+nala install fonts-font-awesome -y
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/* /home/$username/.fonts
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors
+./install.sh
+cd $builddir
+rm -rf Nordzy-cursors
+
+#Install Brave Browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+# Beautiful bash
+git clone https://github.com/ChrisTitusTech/mybash
+cd mybash
+bash setup.sh
+cd $builddir
+
+#Installing gnome-tweak-tools
+sudo apt install gnome-tweak-tool
+
+# Use nala
+bash scripts/usenala
+
+
+
+
+
